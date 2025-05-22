cee6bfc4350404a86e041601d172f72effc4a9ce
Fabian-11 <f.lasso@uniandes.edu.co>
2025-05-21 22:57:01 -0500

    Add files via upload

---
+++ model_deployment1/m10_genre_prediction.py
@@ -0,0 +1,19 @@
+
+import os
+import joblib
+import pandas as pd
+
+BASE_DIR = os.path.abspath('model_deployment1')
+clf = joblib.load(os.path.join(BASE_DIR, 'genre_clf.pkl'))
+vect = joblib.load(os.path.join(BASE_DIR, 'genre_vectorizer.pkl'))
+le = joblib.load(os.path.join(BASE_DIR, 'genre_binarizer.pkl'))
+
+def predict_genres(plot: str, threshold: float = 0.3) -> list:
+    X_new = vect.transform([plot])
+    proba = clf.predict_proba(X_new)[0]
+    return [genre for genre, p in zip(le.classes_, proba) if p >= threshold]
+
+def predict_genres_proba(plot: str) -> dict:
+    X_new = vect.transform([plot])
+    proba = clf.predict_proba(X_new)[0]
+    return dict(zip(['p_' + g for g in le.classes_], proba))
