4778c9eaab473153a90283ea3679b2b8e5e15700
Fabian-11 <f.lasso@uniandes.edu.co>
2025-05-22 22:12:40 -0500

    Update movie_genre_api.py

--- model_deployment1/movie_genre_api.py
+++ model_deployment1/movie_genre_api.py
@@ -45,4 +45,4 @@ class GenrePredictionApi(Resource):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5001)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
